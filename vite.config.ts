4c814ccc4c67a1275b8dbe22315f21596ae3bed2
Pawan Paudel <pawanpaudel93@gmail.com>
2023-09-01 21:53:06 +0545

    fix: Update node polyfills include

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), nodePolyfills()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      include: ["buffer", "crypto", "stream", "util"],
+    }),
+  ],
 });
